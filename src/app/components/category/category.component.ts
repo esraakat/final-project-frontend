2836a18818b6022c17c2983d58d749d3af19dceb
Esra Akat <383196@ogr.ktu.edu.tr>
2022-10-03 15:44:38 +0300

    03/10/2022

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent implements OnInit {
+export class CategoryComponent implements OnInit { 
 
   constructor() { }
 
